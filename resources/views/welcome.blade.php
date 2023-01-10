dad016227dad2ec7af84126f5aa1da00001a7ed9
Fabio Camargo <fabiorcamargo@gmail.com>
2023-01-10 20:01:56 -0300

    Up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
+        <script src="https://core.cademi.com.br/assets/js/remote.js"></script>
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
