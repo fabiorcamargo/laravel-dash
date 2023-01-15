e411a6405ff90a152db9187308a7fc23bffc4d36
Fabio Camargo <fabiorcamargo@gmail.com>
2023-01-15 01:52:56 -0300

    Up First

--- resources/views/pages/aluno/first.blade.php
+++ resources/views/pages/aluno/first.blade.php
@@ -168,8 +168,8 @@
                                             <input type="password" class="form-control invalid" name="password" id="password" placeholder="******" autocomplete="on">
                                             
                                             <label for="name">Repita a Senha</label>
-                                            <input type="password" class="form-control mb-2" name="password2" id="password2" placeholder="******" autocomplete="on" onchange="myPw()">
-                                            <div class="text-danger invisible" name="feed" id="feed" >As Senhas não são iguais!</div>
+                                            <input type="password" class="form-control mb-2" name="password2" id="password2" placeholder="******" autocomplete="on" oninput ="myPw()">
+                                            <div class="text-warning invisible" name="feed" id="feed" >As Senhas não são iguais!</div>
                                             <a id="customCheck1" class="mt-2" onclick="myFunction()"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg></a>
                                              
                                         </div>
