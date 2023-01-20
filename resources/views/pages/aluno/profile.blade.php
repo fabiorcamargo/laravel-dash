6cb3e25a62fb07bff48020a57b2936d53af16caa
Fabio Camargo <fabiorcamargo@gmail.com>
2023-01-19 21:17:40 -0300

    Up Profile

--- resources/views/pages/aluno/profile.blade.php
+++ resources/views/pages/aluno/profile.blade.php
@@ -40,7 +40,7 @@
                     </div>
                     <div class="text-center user-info">
                         <img src="{{ asset($user->image) }}" alt="avatar">
-                        <p class="">{{ $user->name }} {{ $user->lastname }}</p>
+                        <p class="">{{ $user->id }} | {{ $user->name }} {{ $user->lastname }}</p>
                     </div>
                     <div class="user-info-list">
 
@@ -68,7 +68,7 @@
                                 <li class="contacts-block__item">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone me-3"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg> {{ $user->cellphone2 }}
                                 </li>
-                                
+
                                 @isset($cademi->login_auto)
                                 <li class="contacts-block__item">
                                    <button class="btn btn-secondary  _effect--ripple waves-effect waves-light">
