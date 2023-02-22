089d1e654b9982564bbc4abe863ef034da3d6538
Fabio Camargo <fabiorcamargo@gmail.com>
2023-02-22 15:40:40 -0300

    Up profile

--- resources/views/pages/aluno/profile.blade.php
+++ resources/views/pages/aluno/profile.blade.php
@@ -94,7 +94,7 @@
                 <div class="widget-content widget-content-area">
                     <div class="d-flex justify-content-between">
                         <h3 class="">Perfil do Usuário</h3>
-                        {{--<a href="{{getRouterValue();}}/aluno/profile/{{ $user->id }}/edit" class="mt-2 edit-profile"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3"><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg></a>--}}
+                        <a href="{{getRouterValue();}}/aluno/profile/{{ $user->id }}/edit" class="mt-2 edit-profile"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3"><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg></a>
                     </div>
                     
                     <div class="text-center user-info">
@@ -221,11 +221,11 @@
                 <div class="widget-content widget-content-area">
 
                     <div class="d-flex justify-content-between">
-                        <h3 class="">Cursos Liberados</h3>
-                        <a href="{{getRouterValue();}}/app/user/profile/{{ $user->id }}/courses" class="mt-2 edit-profile"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="green" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3"><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg></a>
+                        <h3 class="md-2">Cursos Liberados</h3>
+                        {{--<a href="{{getRouterValue();}}/app/user/profile/{{ $user->id }}/courses" class="mt-2 edit-profile"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="green" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3"><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg></a>--}}
                     </div>
 
-                    <div id="toggleAccordion" class="accordion">
+                    <div id="toggleAccordion" class="accordion mt-4">
                         @foreach ($courses as $course)
                             <div class="card">
                                 <div class="card-header" id="...">
