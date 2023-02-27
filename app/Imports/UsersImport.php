298b1f17ceb5871a414f5008636c59dbb5f85391
Fabio Camargo <fabiorcamargo@gmail.com>
2023-02-27 18:22:43 -0300

    Up queue errors

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -4,6 +4,7 @@ namespace App\Imports;
 
 use App\Http\Controllers\CademiController;
 use App\Models\Cademi;
+use App\Models\CademiImport;
 use App\Models\City;
 use App\Models\State;
 use App\Models\User;
@@ -13,15 +14,18 @@ use Illuminate\Support\Facades\Storage;
 use Maatwebsite\Excel\Concerns\Importable;
 use Maatwebsite\Excel\Concerns\RegistersEventListeners;
 use Maatwebsite\Excel\Concerns\SkipsEmptyRows;
+use Maatwebsite\Excel\Concerns\SkipsOnFailure;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithBatchInserts;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithUpserts;
+use Maatwebsite\Excel\Concerns\WithValidation;
 use Maatwebsite\Excel\Events\AfterImport;
+use Maatwebsite\Excel\Validators\Failure;
 
-class UsersImport implements ToModel, WithChunkReading, WithHeadingRow, WithUpserts, SkipsEmptyRows
+class UsersImport implements ToModel, SkipsEmptyRows, WithChunkReading, WithHeadingRow, WithUpserts, WithValidation, SkipsOnFailure
 {
 
     
@@ -34,16 +38,20 @@ class UsersImport implements ToModel, WithChunkReading, WithHeadingRow, WithUpse
 
     public function model(array $row)
     {
+
+        if ($row['username'] == null) {
+            return null;
+          }
+         
         //dd($_COOKIE['city']);
         $city2 = (preg_replace('/[^0-9]/', '', $_COOKIE['city']));
         $state2 = (City::find($city2)->state_id);
         $state2 = State::find($state2)->abbr;
         $city2 = (City::find($city2)->name);
         
-                
-        
 
         $s = count($row);
+
         $user = User::where('username', $row['username'])->first();
         $r = str_replace(" ", "", $row['courses']);
         $courses = explode(",",  $r);
@@ -117,4 +125,48 @@ class UsersImport implements ToModel, WithChunkReading, WithHeadingRow, WithUpse
     {
         return 'username';
     }
+
+    public function rules(): array
+            {
+                return [
+                    "username" => 'required|string',
+                    "email2" => 'required|string',
+                    "name" => 'required|string',
+                    "lastname" => 'required|string',
+                    "password" => 'required|string',
+                    "role" => 'required|numeric',
+                    "cellphone2" => 'required|numeric',
+                    "city2" => 'required|numeric',
+                    "payment" => 'required|string',
+                    "ouro" => 'required|numeric',
+                    "secretary"  => 'required|string',
+                    "document"  => 'required|string',
+                    "seller"  => 'required|string',
+                    "observation"  => 'required|string',
+                    "courses"  => 'required|string',
+                    "active"  => 'required|numeric',
+
+                    
+                    // so on
+                ];
+            }
+
+public function onFailure(Failure ...$failures)
+{
+    $fails = (json_encode($failures[0]));
+    $fail = (json_decode($fails));
+    //dd($fail);
+                $import = new CademiImport();
+                $import->username = $fail->values->username;
+                $import->status = "error";
+                $import->code = $fail->attribute;
+                $import->msg = $fail->errors[0];
+                $import->body = '{"success": true,
+                    "code": 200,
+                    "data": [{
+                                "status": "Debug"
+                            }]
+                        }';
+                $import->save();
+}
 }
