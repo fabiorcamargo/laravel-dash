73140a9d12372caead9aab3422f6cd1204696e3b
Fabio Camargo <fabiorcamargo@gmail.com>
2023-03-01 19:06:35 -0300

    Up view

--- app/Http/Controllers/ConversionApiFB.php
+++ app/Http/Controllers/ConversionApiFB.php
@@ -74,4 +74,63 @@ class ConversionApiFB extends Controller
             unset($url);
             //exit();
     }
+
+    public function Lead(){
+
+        function geraid() {
+            $data = openssl_random_pseudo_bytes(16);
+        
+            $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+            $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+        
+            return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+          }
+
+            $tempo = time();
+            $page = url()->current();
+            $eventid = geraid();
+
+            $access_token = env('CONVERSIONS_API_ACCESS_TOKEN');
+            $pixel_id = env('CONVERSIONS_API_PIXEL_ID');
+
+            $api = Api::init(null, null, $access_token);
+            $api->setLogger(new CurlLogger());
+
+            $user_data = (new UserData())  
+            ->setEmail((auth()->user()->email))
+            ->setPhone((auth()->user()->cellphone))
+            ->setLastName((auth()->user()->lastname))
+            ->setFirstName((auth()->user()->name))/*
+            ->setCities(array("08809a7d1404509f5ca572eea923bad7c334d16bf92bb4ffc1e576ef34572176"))
+            ->setStates(array("0510eddd781102030eb8860671503a28e6a37f5346de429bdd47c0a37c77cc7d"))
+            ->setCountryCodes(array("885036a0da3dff3c3e05bc79bf49382b12bc5098514ed57ce0875aba1aa2c40d"))*/
+            ->setClientIpAddress($_SERVER['REMOTE_ADDR'])
+            ->setClientUserAgent($_SERVER['HTTP_USER_AGENT']);
+            //->setFbc($fbclick);
+
+            $event = (new Event())
+            ->setEventName("Lead")
+            ->setEventTime($tempo)
+            ->setUserData($user_data)
+            //->setCustomData($custom_data)
+            //->setActionSource("website")
+            ->setEventSourceUrl($page)
+            ->setEventId($eventid);
+                
+            $events = array();
+            array_push($events, $event);
+
+            $request = (new EventRequest($pixel_id))
+                ->setTestEventCode('TEST9910')
+                ->setEvents($events);
+            $response = $request->execute();
+            //dd($response);
+
+            //header('Location: ' . $url, true, $permanent ? 301 : 302);
+
+            unset($pixel);
+            unset($token);
+            unset($url);
+            //exit();
+    }
 }
--- resources/views/components/base-layout.blade.php
+++ resources/views/components/base-layout.blade.php
@@ -48,7 +48,7 @@
         fbq('track', 'ViewContent');
         </script>
         <noscript><img height="1" width="1" style="display:none"
-        src="https://www.facebook.com/tr?id=881551146233401&ev=PageView&noscript=1"
+        src="https://www.facebook.com/tr?id=881551146233401&ev=ViewContent&noscript=1"
         /></noscript>
         <!-- End Meta Pixel Code -->
     </script>
