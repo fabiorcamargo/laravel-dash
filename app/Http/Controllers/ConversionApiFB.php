5328a93d03a6423d2054a2980074dddd51afc810
Fabio Camargo <fabiorcamargo@gmail.com>
2023-03-01 19:08:23 -0300

    Up view

--- app/Http/Controllers/ConversionApiFB.php
+++ app/Http/Controllers/ConversionApiFB.php
@@ -16,20 +16,20 @@ use FacebookAds\Object\ServerSide\UserData;
 
 class ConversionApiFB extends Controller
 {
-    public function Lead(){
+    public function geraid() {
+        $data = openssl_random_pseudo_bytes(16);
+    
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+    
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+      }
 
-        function geraid() {
-            $data = openssl_random_pseudo_bytes(16);
-        
-            $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
-            $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
-        
-            return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
-          }
+    public function Lead(){
 
             $tempo = time();
             $page = url()->current();
-            $eventid = geraid();
+            $eventid = ConversionApiFB::geraid();
 
             $access_token = env('CONVERSIONS_API_ACCESS_TOKEN');
             $pixel_id = env('CONVERSIONS_API_PIXEL_ID');
@@ -77,18 +77,9 @@ class ConversionApiFB extends Controller
 
     public function ViewContent(){
 
-        function geraid() {
-            $data = openssl_random_pseudo_bytes(16);
-        
-            $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
-            $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
-        
-            return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
-          }
-
             $tempo = time();
             $page = url()->current();
-            $eventid = geraid();
+            $eventid = ConversionApiFB::geraid();
 
             $access_token = env('CONVERSIONS_API_ACCESS_TOKEN');
             $pixel_id = env('CONVERSIONS_API_PIXEL_ID');
