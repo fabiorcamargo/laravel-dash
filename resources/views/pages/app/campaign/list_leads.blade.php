41898d7fda8037e7137152b4a5978f73a9a50e36
Fabio Camargo <fabiorcamargo@gmail.com>
2023-03-03 19:14:34 -0300

    Up list

--- resources/views/pages/app/campaign/list_leads.blade.php
+++ resources/views/pages/app/campaign/list_leads.blade.php
@@ -83,21 +83,19 @@
                             <thead>
                             <tr>
                                 <th scope="col">Data</th>
-                                <th scope="col">Username</th>
                                 <th scope="col">Nome</th>
+                                <th scope="col">Username</th>
                                 {{--<th scope="col">Email</th>--}}
                                 <th scope="col">Telefone</th>
-                                <th scope="col">Cidade - UF</th>
                                 <th class="text-center">Image</th>
-                                <th class="text-center">Pagamento</th>
-                                <th class="text-center dt-no-sorting">Ação</th>
                             </tr>
                         </thead>
                         <tbody>
                             
                             @foreach ($users as $user)
                             <tr>
-                                <td>{{ $user->created_at->format('d/m/Y h:m:i') }}</td>
+                                <td>{{ $user->created_at->format('d/m/y h:m') }}</td>
+                                <td>{{ $user->name }} {{ $user->lastname }}</td>
                                 <td>{{ $user->username }} 
                                     @if($user->first == 2)
                                     <div class="badge badge-success badge-dot"></div>
@@ -105,38 +103,14 @@
                                     <div class="badge badge-danger badge-dot"></div>
                                     @endif
                                 </td>
-                                <td>{{ $user->name }} {{ $user->lastname }}</td>
                                 {{--<td>{{ $user->email }}</td>--}}
                                 <td>{{ $user->cellphone }}</td>
-                                <td>{{ $user->city }} - {{ $user->uf }}</td>
                                 <td>
                                     <div class="d-flex">
                                         <div class="usr-img-frame mr-2 rounded-circle">
                                             <span><img src="{{ asset($user->image) }}" class="rounded-circle profile-img" alt="avatar"></span>
                                         </div>
                                     </div>
-                                </td>
-
-                                @if ($user->payment == "CARTÃO")
-                                <td class="text-center"><span class="shadow-none badge badge-success">Cartão</span></td>
-                                @elseif ($user->payment == "BOLETO")
-                                <td class="text-center"><span class="shadow-none badge badge-primary">Boleto</span></td>
-                                @else
-                                <td class="text-center"><span class="shadow-none badge badge-dark">Vazio</span></td>
-                                @endif
-
-                                <td class="text-center">
-                                        <div class="action-btns">
-                                            <a href="{{ getRouterValue(); }}/app/user/profile/{{ $user->id }}" class="action-btn btn-view bs-tooltip me-2" data-toggle="tooltip" data-placement="top" title="Ver">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
-                                            </a>
-                                            <a href="{{ getRouterValue(); }}/app/user/profile/{{ $user->id }}/edit" class="action-btn btn-edit bs-tooltip me-2" data-toggle="tooltip" data-placement="top" title="Editar">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg>
-                                            </a>
-                                            <a href="{{ getRouterValue(); }}/app/user/profile/{{ $user->id }}/edit" class="action-btn btn-edit bs-tooltip me-2" data-toggle="tooltip" data-placement="top" title="Pagamentos">
-                                                <x-widgets._w-svg svg="cash-banknote"/>
-                                            </a>
-                                        </div>
                                 </td>
                             </tr>
                             @endforeach
