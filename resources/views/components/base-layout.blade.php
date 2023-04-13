1b65ed0c9e853a968a6ad78bf23d0d7a9f0a8dd9
Fabio Camargo <fabiorcamargo@gmail.com>
2023-04-13 14:57:36 -0300

    up

--- resources/views/components/base-layout.blade.php
+++ resources/views/components/base-layout.blade.php
@@ -19,7 +19,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
-    <meta name="facebook-domain-verification" content="ba30vae8y5ydyhox0nuqbo6swan5ci" />
+    
     <title>{{ $pageTitle }}</title>
     <link rel="icon" type="image/x-icon" href="{{Vite::asset('resources/images/favicon.ico')}}"/>
     @vite(['resources/scss/layouts/vertical-light-menu/light/loader.scss'])
