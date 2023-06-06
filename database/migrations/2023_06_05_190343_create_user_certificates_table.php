f429971767f730488c90fb26ad1dd0e014f2594c
Fabio Camargo <fabiorcamargo@gmail.com>
2023-06-06 17:19:37 -0300

    up

--- database/migrations/2023_06_05_190343_create_user_certificates_table.php
+++ database/migrations/2023_06_05_190343_create_user_certificates_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained('users');
             $table->string('name');
             $table->string('hours');
-            $table->string('hours');
             $table->timestamps();
         });
     }
