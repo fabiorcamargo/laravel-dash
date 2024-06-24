48fb64c4291c1111b99efedb93b86f47f6f3eab3
Fabio Camargo <fabiorcamargo@gmail.com>
2024-06-24 19:07:32 -0300

    up

--- app/Http/Controllers/ApiGetOuroCourses.php
+++ app/Http/Controllers/ApiGetOuroCourses.php
@@ -12,18 +12,13 @@ class ApiGetOuroCourses extends Controller
         $user = $request->user();
         $ouro = $user->client_ouro->first();
 
-        $OuroCourses = [];
+        
 
         if ($user->client_ouro->first()) {
-            $courses = $user->client_ouro->first()->matricula_ouro;
-            foreach ($courses as $course) {
-                if ($course->get_course) {
-                    $OuroCourses[] = $course->get_course;
-                }
-            }
+           $user->client_ouro->first()->matricula_ouro;
         }
 
 
-        return response()->json(['courses' => $OuroCourses, 'ouroClient' => $ouro]);
+        return response()->json(['ouroClient' => $ouro]);
     }
 }
