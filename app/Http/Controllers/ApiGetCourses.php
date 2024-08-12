436a2a525358f2fc7bb0b9f5cf7c07142b5daa40
Fabio Camargo <fabiorcamargo@gmail.com>
2024-08-12 17:51:31 -0300

    up

--- app/Http/Controllers/ApiGetCourses.php
+++ app/Http/Controllers/ApiGetCourses.php
@@ -42,7 +42,7 @@ class ApiGetCourses extends Controller
 
         if($user->role == 4){
 
-            $cademiCourses = CademiListCourse::get()->pluck('name');
+            $cademiCourses = CademiListCourse::get();
 
         }
 
